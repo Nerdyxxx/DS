606053d86b7af3dcdc5f0438e365e1569b75c5c8
Eugene Nagan <zheka2016jk@mail.ru>
2023-03-19 23:54:34 +0300

    Data sctructure 1 labratory С++

---
+++ Labratory1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+using namespace std;
+
+const int MAXN = 2e9;
+
+int main() {
+    
+    int a, b;
+    cin >> a >> b;
+
+    if (a < 2 || b > MAXN) {
+        cout << "Неверное значение диапазон => (2<=A; B <=2 * 10^9)\n";
+        return 0;
+    }
+    auto start = chrono::high_resolution_clock::now();
+    int k = 1;
+    if (b % a == 0) {
+        cout << k;
+        return 0;
+    }
+    k++;
+    while (pow(b, k) <= MAXN) {
+        if (fmod(pow(b, k), a) == 0) {
+            cout << k;
+            break;
+        } else {
+            k++;
+        }
+    }
+    if (pow(b, k) > MAXN) {
+        cout << "-1";
+    }
+    cout << endl;
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+    cout << "Время: " << duration.count() << " ms\n";
+    return 0;
+}
